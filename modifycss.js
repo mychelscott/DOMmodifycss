a83ea1a42f91e0279a99c7c5d5c0a916a6b28db0
mychelscott <scottmychel@gmail.com>
2023-11-22 22:37:29 -0800

    html and js code added for modified css using DOM + event listener

---
+++ modifycss.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //change bg color of this div to yellow (example 1)
+    document.getElementById('example 1').style.backgroundColor = 'yellow';
+
+    //increase font size of paragraph to 24px (example 2)
+    document.getElementById('example2').style.fontSize = '24px';
+
+    //make text color of this heading red(example3)
+    document.getElementById('example3').style.color = 'red';
+
+    //change button`s border to a solid black border
+    document.getElementById('example4').style.border = '2px solid black';
+
+    //set a new background image for this section
+    document.getElementById('example5').style.backgroundImage = "url('https://www.pexels.com/photo/blue-and-purple-cosmic-sky-956999/')";
+
+    //hide this paragraph by setting it`s display property to `none`
+    document.getElementById('example6').style.display = 'none';
+
+    //change the font color of this list to green
+    document.getElementById('example7').style.color = 'green';
+});
+
+console.log ()
